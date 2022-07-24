eb5fda0f077a519e663e73158f1937f580e15433
Forest Carter <fc_vstate@yahoo.com>
2022-07-24 18:13:54 -0400

    update feature/generate-webpage

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
+//receives written html file
+const fs = require('fs');
 
-var commandLineArgs = process.argv;
-
-console.log(commandLineArgs);
+//receives page-template
+const generatePage = require('./src/page-template');
 
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-console.log(profileDataArgs);
-
-const printProfileData = profileDataArr => {
-    //This....
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('================');
-
-    //Is the same as this....
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-    
-};
+const [name, github] = profileDataArgs;
 
 
-printProfileData(profileDataArgs);
+//writes html to file
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
+
+
