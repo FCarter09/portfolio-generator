05fc8d2cebe04df2f00d17589850cacbd6183d93
Forest Carter <fc_vstate@yahoo.com>
2022-07-24 20:31:22 -0400

    update feature/questions

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
+//receives inquirer file
+const inquirer = require('inquirer');
+
+// question and text input answer
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is your name?"
+        }
+    ])
+
+    .then(answers => console.log(answers));
 //receives written html file
-const fs = require('fs');
+// const fs = require('fs');
 
-//receives page-template
-const generatePage = require('./src/page-template');
+// //receives page-template
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
 
-//writes html to file
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// //writes html to file
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
 
