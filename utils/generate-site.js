924bc115dc6a00718f32babb0b09d6ee8a4e030d
FCarter09 <fc_vstate@yahoo.com>
2023-12-09 11:22:02 -0500

    index.html moved from dist folder to root folder

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -4,7 +4,7 @@ const writeFile = fileContent => {
 
     return new Promise((resolve, reject) => {
 
-        fs.writeFile('./dist/index.html', fileContent, err => {
+        fs.writeFile('../index.html', fileContent, err => {
             // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
             if (err) {
             reject(err);
